32590b1e7056ac1c302fff23b30a7d066ebac608
Laksamadi Guko <elru.lakusa@gmail.com>
2018-01-08 08:35:48 +0700

    Add files via upload

--- mikhmon/app/conntest.php
+++ mikhmon/app/conntest.php
@@ -8,9 +8,6 @@ if(!isset($_SESSION['usermikhmon'])){
 error_reporting(0);
 require('./lib/api.php');
 include('./config.php');
-if ($reloadindex == "0"){ 
-} else
-header("Refresh: $reloadindex ; url='./'");
 $API = new RouterosAPI();
 $API->debug = false;
 if ($API->connect( $iphost, $userhost, $passwdhost )) {
@@ -26,7 +23,7 @@ if ($API->connect( $iphost, $userhost, $passwdhost )) {
 		<meta http-equiv="pragma" content="no-cache" />
 		<meta http-equiv="refresh" content="" />
 		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;"/>
-		<link rel="icon" href="favicon.ico" />
+		<link rel="icon" href="./img/favicon.png" />
 		<link rel="stylesheet" href="css/style.css" media="screen">
 		<script>
 			function Reload() {
@@ -58,13 +55,53 @@ if ($API->connect( $iphost, $userhost, $passwdhost )) {
 								if ($cek == ""){
 									 ?><meta http-equiv="refresh" content="0; url=setup.php"><?php
 								}
-										for ($i=0; $i<$TotalReg; $i++){$regtable = $ARRAY[$i];echo "Platform : " . $regtable['platform'] . "<br />";}echo "</td><tr><td>"; 
-										for ($i=0; $i<$TotalReg; $i++){$regtable = $ARRAY[$i];echo "Board Name : " . $regtable['board-name'] . "<br />";}echo "</td></tr><td>";
-										for ($i=0; $i<$TotalReg; $i++){$regtable = $ARRAY[$i];echo "Version : " . $regtable['version'] . "<br />";}echo "</td>";
+										$regtable = $ARRAY[0];echo "Platform <td>:</td><td>" . $regtable['platform'] . "<br />";echo "</td><tr><td>"; 
+										$regtable = $ARRAY[0];echo "Board Name <td>:</td><td>" . $regtable['board-name'] . "<br />";echo "</td></tr><td>";
+										$regtable = $ARRAY[0];echo "Version <td>:</td><td>" . $regtable['version'] . "<br />";echo "</td></tr><td>";
+										$regtable = $ARRAY[0];echo "CPU Load<td>:</td><td>" . $regtable['cpu-load'] . "%<br />";echo "</td></tr><td>";
+										$regtable = $ARRAY[0];echo "Free Memory <td>:</td><td>" . formatBytes2($regtable['free-memory'],0) . "<br />";echo "</td></tr><td>";
+										$regtable = $ARRAY[0];echo "Free HDD Space <td>:</td><td>" . formatBytes2($regtable['free-hdd-space'],0) . "<br />";echo "</td>";
 							?>
 				</tr>
 			</table>
 		</div>
 	</body>
+<?php
+
+function formatBytes($bytes, $precision = 2) { 
+$units = array('B', 'KB', 'MB', 'GB', 'TB'); 
+
+$bytes = max($bytes, 0); 
+$pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
+$pow = min($pow, count($units) - 1); 
+
+// Uncomment one of the following alternatives
+// $bytes /= pow(1024, $pow);
+// $bytes /= (1 << (10 * $pow)); 
+
+return round($bytes, $precision) . ' ' . $units[$pow]; 
+} 
+
+function formatBytes2($size, $decimals = 0){
+$unit = array(
+'0' => 'Byte',
+'1' => 'KiB',
+'2' => 'MiB',
+'3' => 'GiB',
+'4' => 'TiB',
+'5' => 'PiB',
+'6' => 'EiB',
+'7' => 'ZiB',
+'8' => 'YiB'
+);
+
+for($i = 0; $size >= 1024 && $i <= count($unit); $i++){
+$size = $size/1024;
+}
+
+return round($size, $decimals).' '.$unit[$i];
+}
+
+?>
 </html>
 
